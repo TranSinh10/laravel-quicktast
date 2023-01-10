0a46586c3c1408afa705e9f8a72940c333e1c617
TranSinh10 <80934375+TranSinh10@users.noreply.github.com>
2023-01-09 18:38:24 -0800

    Update 2023_01_09_212821_create_tasks_table.php

--- database/migrations/2023_01_09_212821_create_tasks_table.php
+++ database/migrations/2023_01_09_212821_create_tasks_table.php
@@ -17,8 +17,8 @@ class CreateTasksTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->string('description');
-            $table->unsignedBigInteger('id_user');
-            $table->foreign('id_user')->references('id')->on('users');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
